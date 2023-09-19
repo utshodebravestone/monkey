9c59089fe32e57c74f37fcb7a5b26cf620ce4555
utsho <utshodebravestone@gmail.com>
2023-09-19 22:26:37 +0600

    added cmd/monkey

---
+++ cmd/monkey.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"monkey/lexer"
+	"monkey/token"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("welcome to the monkey programming language\n")
+	for {
+		fmt.Print("input :> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+
+		l := lexer.New(input)
+		for t := l.NextToken(); t.Kind != token.EOF; t = l.NextToken() {
+			fmt.Println(t.ToString())
+		}
+	}
+}

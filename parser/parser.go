16ec6dcc52597a7797fb59f492e6799eef2c3e21
utsho <utshodebravestone@gmail.com>
2023-09-28 21:57:53 +0600

    added parser's basic structute

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,32 @@
 package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken() // loading currentToken
+	p.nextToken() // loading peekToken
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,1 +1,23 @@
 package parser
+
+import (
+	"monkey/assert"
+	"monkey/lexer"
+	"monkey/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `foo bar`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	assert.Equal(t, p.currentToken.Kind, token.IDENTIFIER)
+	assert.Equal(t, p.currentToken.Lexeme, "foo")
+
+	p.nextToken()
+
+	assert.Equal(t, p.currentToken.Kind, token.IDENTIFIER)
+	assert.Equal(t, p.currentToken.Lexeme, "bar")
+}

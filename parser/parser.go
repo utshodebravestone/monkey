772dc3e9494e205983a91981b9b5fab0bed5dbd6
utsho <utshodebravestone@gmail.com>
2023-09-28 22:47:14 +0600

    added let statement

--- parser/parser.go
+++ parser/parser.go
@@ -27,6 +27,66 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
+func (p *Parser) currentTokenIs(kind token.TokenKind) bool {
+	return p.currentToken.Kind == kind
+}
+
+func (p *Parser) peekTokenIs(kind token.TokenKind) bool {
+	return p.peekToken.Kind == kind
+}
+
+func (p *Parser) expectPeek(kind token.TokenKind) bool {
+	if p.peekTokenIs(kind) {
+		p.nextToken()
+		return true
+	}
+
+	return false
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currentToken.Kind {
+	case token.LET:
+		return p.parseLetStatement()
+
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.currentToken}
+
+	if !p.expectPeek(token.IDENTIFIER) {
+		return nil
+	}
+
+	stmt.Name = &ast.IdentifierExpression{
+		Token: p.currentToken,
+	}
+
+	if !p.expectPeek(token.EQUAL) {
+		return nil
+	}
+
+	for !p.currentTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{
+		Statements: []ast.Statement{},
+	}
+
+	for p.currentToken.Kind != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+	}
+
+	return program
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `foo bar`
+	input := `foo 1`
 
 	l := lexer.New(input)
 	p := New(l)
@@ -18,6 +18,34 @@ func TestNextToken(t *testing.T) {
 
 	p.nextToken()
 
-	assert.Equal(t, p.currentToken.Kind, token.IDENTIFIER)
-	assert.Equal(t, p.currentToken.Lexeme, "bar")
+	assert.Equal(t, p.currentToken.Kind, token.NUMBER)
+	assert.Equal(t, p.currentToken.Lexeme, "1")
+}
+
+func TestAssertionMethods(t *testing.T) {
+	input := `foo 1`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	assert.Equal(t, p.currentTokenIs(token.IDENTIFIER), true)
+	assert.Equal(t, p.peekTokenIs(token.NUMBER), true)
+
+	assert.Equal(t, p.expectPeek(token.NUMBER), true)
+
+	assert.Equal(t, p.currentTokenIs(token.NUMBER), true)
+	assert.Equal(t, p.peekTokenIs(token.EOF), true)
+}
+
+func TestParseLetStatement(t *testing.T) {
+	input := `let foo = bar;`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	actual := p.parseLetStatement()
+
+	assert.Equal(t, token.LET, actual.Token.Kind)
+	assert.Equal(t, token.IDENTIFIER, actual.Name.Token.Kind)
+	assert.Equal(t, "foo", actual.Name.Token.Lexeme)
 }
